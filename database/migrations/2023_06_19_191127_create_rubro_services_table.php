54e749d212d0d80fe100d6214187a945bb6909fc
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-06-22 13:42:11 -0500

    cambios

---
+++ database/migrations/2023_06_19_191127_create_rubro_services_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRubroServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rubro_services', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_rubro')->default(0)->nullable();
+            $table->string('rubro');
+            $table->string('title');
+            $table->string('text')->nullable()->default('');
+            $table->string('photo')->nullable()->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rubro_services');
+    }
+}
