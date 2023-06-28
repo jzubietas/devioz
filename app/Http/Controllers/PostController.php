7f1af5ccfe6bae8fbe6c27c8afdaa02c417ae6f3
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-06-27 22:30:05 -0500

    carga

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -110,25 +110,17 @@ class PostController extends Controller
     {
         $this->validate($request, [
             'name' => 'required',
-            'email' => 'required|email|unique:users,email,'.$id,
-            'password' => 'same:confirm-password',
-            'roles' => 'required'
         ]);
 
         $input = $request->all();
-        if(!empty($input['password'])){
-            $input['password'] = Hash::make($input['password']);
-        }else{
-            $input = Arr::except($input,array('password'));
-        }
 
-        $user = User::find($id);
-        $user->update($input);
-        DB::table('model_has_roles')->where('model_id',$id)->delete();
+        $post = Post::find($id);
+        $post->update($input);
+        //DB::table('model_has_roles')->where('model_id',$id)->delete();
 
-        $user->assignRole($request->input('roles'));
+        //$post->assignRole($request->input('roles'));
 
-        return redirect()->route('usuarios.index');
+        return redirect()->route('posts.index');
     }
 
     /**
