691a93b8170aac268317a82c76167f27b89503a6
jzubietas <jhonathanisaizubietasantos@gmail.com>
2023-06-27 23:12:31 -0500

    carga

--- resources/views/usuarios/show.blade.php
+++ resources/views/usuarios/show.blade.php
@@ -4,29 +4,29 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2> Show User</h2>
+                <h2> Detalle de Usuario</h2>
             </div>
             <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('usuarios.index') }}"> Back</a>
+                <a class="btn btn-primary" href="{{ route('usuarios.index') }}"> Volver</a>
             </div>
         </div>
     </div>
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
+                <strong>Nombre:</strong>
                 {{ $user->name }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Email:</strong>
+                <strong>Correo:</strong>
                 {{ $user->email }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Roles:</strong>
+                <strong>Rol:</strong>
                 @if(!empty($user->getRoleNames()))
                     @foreach($user->getRoleNames() as $v)
                         <label class="badge badge-success">{{ $v }}</label>
